9cadf0163fde17895b19453c6240282eea7941b0
Ankit <ankitbansal@berkeley.edu>
2019-08-21 19:41:23 -0700

    First_commit

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1hdbUe48iPhtsGX86SRPVOGneyPQ0bghQ'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['burned', 'normal']
 path = Path(__file__).parent
 
 app = Starlette()
